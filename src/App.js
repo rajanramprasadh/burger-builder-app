c79abac90fe9dcea0d98e0854f4bee7f9258012e
rajanramprasadh <rajanramprasadh@gmail.com>
2021-04-16 17:44:06 +0530

    Done till Adding a Dynamic Ingredient Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     );
   }
 }
